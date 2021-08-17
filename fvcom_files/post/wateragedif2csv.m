5e391a5dade6fab736a164626897ec3ee42c0f36
dlxiii <tokyowangyulong@gmail.com>
2021-08-17 10:30:10 +0900

    update

--- fvcom_files/post/wateragedif2csv.m
+++ fvcom_files/post/wateragedif2csv.m
@@ -26,11 +26,11 @@ formatSpec = '%02i';
 td = load("./wa_dif.mat");
 wa_dif = td.wa_dif;
 clear td;
-csv_resol = 1000;
 
 %% present monthly layers
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+csv_resol = 1000;
 layerlist = [1,8,16];
 month = {'m09','m10','m11','m12',...
     'm01','m02','m03','m04','m05','m06',...
@@ -40,7 +40,7 @@ caselist = {'c0001',...
     'c1001','c1003',...
     'c2001','c2003'};
 % c=1;l=1;layer=layerlist(l);m = 1;
-otfile_bash = ['../../csv_files/multi_run.sh'];
+otfile_bash = ['../../csv_files/multi_run_m.sh'];
 fileID0 = fopen(otfile_bash,'w');
 fprintf(fileID0,'#!/bin/bash\n');
 for c = 1:length(caselist)
@@ -146,6 +146,102 @@ for c = 1:length(caselist)
             savefig(['./fig/',...
                 caselist{c},'_',month{m},'_',num2str(layer),'.fig']);
             %}
+        end
+    end
+end
+fclose(fileID0);
+
+%% present annual layers
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+csv_resol = 100;
+layerlist = [1,8,16];
+mmean = {'m00'};
+month = {'m09','m10','m11','m12',...
+    'm01','m02','m03','m04','m05','m06',...
+    'm07','m08'};
+caselist = {'c0001',...
+    'c0301','c0303',...
+    'c1001','c1003',...
+    'c2001','c2003'};
+
+% f = 1;m = 1;
+for f = 1:length(caselist)
+    wa_dif.(caselist{f}).m00 = 0;
+    for m = 1:length(month)
+        wa_dif.(caselist{f}).m00 = ...
+            wa_dif.(caselist{f}).m00 + ...
+            wa_dif.(caselist{f}).(month{m});
+    end
+    wa_dif.(caselist{f}).m00 = wa_dif.(caselist{f}).m00 / 12;
+end
+
+otfile_bash = ['../../csv_files/multi_run_a.sh'];
+fileID0 = fopen(otfile_bash,'w');
+fprintf(fileID0,'#!/bin/bash\n');
+for c = 1:length(caselist)
+    for l = 1:length(layerlist)
+        layer = layerlist(l);
+        for m = 1:length(mmean)
+            data1 = double(wa_dif.(caselist{c}).(mmean{m})(:,layer));
+            data1 = filloutliers(data1,'nearest','mean');
+            data_temp.intp = scatteredInterpolant(...
+                wa_dif.lon,...
+                wa_dif.lat,...
+                data1,...
+                'natural');
+            data_temp.lonint = (139.14:(csv_resol/111000):140.39)';
+            data_temp.latint = (34.85:(csv_resol/111000):35.70)';
+            data_temp.ageint = data_temp.intp({data_temp.lonint,data_temp.latint});
+
+            data = data_temp.ageint;
+            data = filloutliers(data,'nearest','mean');
+
+            % Create csv
+            [Y,X] = meshgrid(data_temp.latint,data_temp.lonint);
+            M = [reshape(X,[numel(X),1]),...
+                reshape(Y,[numel(Y),1]),...
+                reshape(data_temp.ageint,[numel(data_temp.ageint),1])];
+            if exist('../../csv_files', 'dir')~=7
+                mkdir('../../csv_files')
+            end
+            filename = [caselist{c},'_',mmean{m},'_',num2str(layer,formatSpec)];
+            otfile_csv = ['../../csv_files/',...
+                 filename,'.csv'];
+            fileID = fopen(otfile_csv,'w');
+            % fprintf(fileID,'%12s %12s %12s\n','lon,','lat,','age,');
+            for i = 1:length(M)
+                fprintf(fileID,'%11.6f%1s %11.6f%1s %11.2f%1s\n',...
+                    M(i,1),',',M(i,2),',',M(i,3),',');
+            end
+            fclose(fileID);
+            
+            % Create vrt
+            otfile_vrt = ['../../csv_files/',...
+                 filename,'.vrt'];
+            fileID = fopen(otfile_vrt,'w');
+            fprintf(fileID,'<OGRVRTDataSource>\n');
+            fprintf(fileID,'    <OGRVRTLayer name="%s">\n',filename);
+            fprintf(fileID,'        <SrcLayer>%s</SrcLayer>\n',filename);
+            fprintf(fileID,'        <LayerSRS>EPSG:4326</LayerSRS>\n');
+            fprintf(fileID,'        <SrcDataSource>CSV:%s.csv</SrcDataSource>\n',filename);
+            fprintf(fileID,'        <GeometryType>wkbPoint</GeometryType>\n');
+            fprintf(fileID,'        <GeometryField encoding="PointFromColumns" x="field_1" y="field_2" z="field_3"/>\n');
+            fprintf(fileID,'    </OGRVRTLayer>\n');
+            fprintf(fileID,'</OGRVRTDataSource>\n');
+            fclose(fileID);
+ 
+            % Create bash
+            otfile_sh = ['../../csv_files/',...
+                 filename,'.sh'];
+            fileID = fopen(otfile_sh,'w');
+            fprintf(fileID,'#!/bin/bash\n');
+            fprintf(fileID,'gdal_grid -l %s -zfield field_3 -a invdistnn:power=2.0:smothing=0.0:radius=%f:max_points=12:min_points=0:nodata=-9999.0 -ot Float32 -of GTiff %s.vrt %s.tif\n',filename,csv_resol*1.2,filename,filename);
+            fclose(fileID);
+            
+            % Create bash
+            fprintf(fileID0,'sh ./%s.sh\n',filename);
+            
         end
     end
 end
