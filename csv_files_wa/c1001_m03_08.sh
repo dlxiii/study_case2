c97b00eff30c89c0b2f8d9942e1e5b62db158ee7
dlxiii <tokyowangyulong@gmail.com>
2021-08-18 14:35:49 +0900

    update water age

--- csv_files_wa/c1001_m03_08.sh
+++ csv_files_wa/c1001_m03_08.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 gdal_grid -l c1001_m03_08 -zfield field_3 -a invdistnn:power=2.0:smothing=0.0:radius=1200.000000:max_points=12:min_points=0:nodata=-9999.0 -ot Float32 -of GTiff c1001_m03_08.vrt c1001_m03_08.tif
+gdal_contour -b 1 -a ELEV -i 1.0 -f "ESRI Shapefile" c1001_m03_08.tif c1001_m03_08.shp
