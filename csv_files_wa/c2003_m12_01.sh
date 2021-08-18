eb6aa0ae81e9ea0cb8f1c3a91753887db4d800fd
dlxiii <tokyowangyulong@gmail.com>
2021-08-18 11:29:13 +0900

    wa update

---
+++ csv_files_wa/c2003_m12_01.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gdal_grid -l c2003_m12_01 -zfield field_3 -a invdistnn:power=2.0:smothing=0.0:radius=1200.000000:max_points=12:min_points=0:nodata=-9999.0 -ot Float32 -of GTiff c2003_m12_01.vrt c2003_m12_01.tif
