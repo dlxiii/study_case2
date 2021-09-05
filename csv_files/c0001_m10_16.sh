af741d3dfd44dcd78edee102bbe61b4c9048f05b
dlxiii <tokyowangyulong@gmail.com>
2021-09-06 01:05:03 +0900

    update

--- csv_files/c0001_m10_16.sh
+++ csv_files/c0001_m10_16.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 gdal_grid -l c0001_m10_16 -zfield field_3 -a invdistnn:power=2.0:smothing=0.0:radius=1200.000000:max_points=12:min_points=0:nodata=-9999.0 -ot Float32 -of GTiff c0001_m10_16.vrt c0001_m10_16.tif
+gdal_contour -b 1 -a ELEV -i 1.0 -f "ESRI Shapefile" c0001_m10_16.tif c0001_m10_16.shp
